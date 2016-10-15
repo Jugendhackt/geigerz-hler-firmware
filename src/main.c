4efbae65b26b3ba6dc32dc22b1cd3cd636593b8d
Markus Schmidl <markus.schmidl@ymail.com>
2016-10-15 13:53:20 +0200

    output values in console

--- src/main.c
+++ src/main.c
@@ -4,37 +4,63 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define TIMER_TIME 1
+#define BAUDRATE B9600
 
-void timer_init(struct timeval &tv, time_t sec)
+void printhelp(char *command)
 {
-	gettimeofday(&tv, NULL);
-	tv.tv_sec += sec;
+	printf("Usage: %s [uartdevice]\n", command);
 }
 
-int check_timer(struct timeval &tv, time_t sec)
+int main(int argc, char *argv[])
 {
-	struct timeval cur_tv;
-	gettimeofday(&cur_tv, NULL);
+	int fd, cps, cpm;
+	struct termios tp;
+	char buffer[100];
+	ssize_t length;
+	float usvhr;
 
-	if (cur_tv.tv_sec > tv.tv_sec) {
-		timer_init(tv, sec);
+	switch (argc) {
+	case 1:
+		printhelp(argv[0]);
+		return 1;
+	case 2:
+		break;
+	default:
+		printhelp(argv[0]);
 		return 1;
 	}
 
-	return 0;
-}
+	fd = open(argv[1], O_RDONLY | O_NOCTTY |O_NDELAY);
+	if (fd < 0) {
+		perror(argv[1]);
+	}
 
-int main(void)
-{
-	struct timeval tv;
+	fcntl(fd, F_SETFL, 0);
+	bzero(&tp, sizeof(tp));
 
-	timer_init(tv, TIMER_TIME);
+	tp.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+	tp.c_iflag = IGNPAR | ICRNL;
+	tp.c_lflag = ICANON;
+
+	tcsetattr(fd, TCSANOW, &tp);
 
 	for (;;) {
-		if (check_timer(tv, TIMER_TIME)) {
-			/* send data */
+		length = read(fd, &buffer, sizeof(buffer));
+
+		if (length < 0) {
+			fprintf(stderr, "Error opening device: %s\n", argv[1]);
+			return -1;
+		}
+		if (length > 0) {
+			buffer[length] = '\0';
+			sscanf(buffer, "CPS, %d, CPM, %d, uSv/hr, %f", &cps, &cpm, &usvhr);
+			printf("cps: %d\ncpm: %d\nuSv/hr: %f\n", cps, cpm, usvhr);
 		}
 	}
+
+	return 1;
 }
